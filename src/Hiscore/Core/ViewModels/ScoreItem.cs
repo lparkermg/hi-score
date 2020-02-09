f55d2a529730c2b0627a2686f59637e60e3de351
Luke Parker <lparkermg@outlook.com>
2020-02-09 20:42:48 +0000

    Documented ScoreItem.

--- src/Hiscore/Core/ViewModels/ScoreItem.cs
+++ src/Hiscore/Core/ViewModels/ScoreItem.cs
@@ -1,9 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <copyright file="ScoreItem.cs" company="Luke Parker">
+// Copyright (c) Luke Parker. All rights reserved.
+// </copyright>
 
-namespace Core.ViewModels
+namespace LPSoft.Hiscore.Core.ViewModels
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Model for a single score.
+    /// </summary>
     public class ScoreItem
     {
     }
